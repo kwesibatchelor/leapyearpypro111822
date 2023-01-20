b77357743ee33cffce75c3562c3d5d12429c1fa8
kbatch <kbatch@kbatchs-MBP.home>
2023-01-19 19:45:27 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def is_leap_year(year):
+    if year %4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print ('Leap Year')
+            else:
+                print ('Not Leap Year')
+        else:
+            print('Leap Year')
+    else:
+        print('Not Leap Year')
+is_leap_year(2016)
